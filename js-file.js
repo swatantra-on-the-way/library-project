a87f6f770619d687f7d19ced53165ea2e5ad3ba7
swatantra-on-the-way <swatantrachhetri3@gmail.com>
2024-07-12 20:40:03 +1000

    Finish final layout

--- js-file.js
+++ js-file.js
@@ -32,30 +32,57 @@ function displayBook(){
         let p1 = document.createElement("p");
         let p2 = document.createElement("p");
         let p3 = document.createElement("p");
-        p1.innerText = `Title: ${book.title}`;
-        p2.innerText = `Author: ${book.author}`;
-        p3.innerText = `Number of Pages: ${book.numberOfPages}`;
+
+
+        let span_a1 = document.createElement("span");
+        let span_a2 = document.createElement("span");
+        let span_b1 = document.createElement("span");
+        let span_b2 = document.createElement("span");
+        let span_c1 = document.createElement("span");
+        let span_c2 = document.createElement("span");
+
+        span_a1.innerText = "TITLE";
+        span_a2.innerText = `${book.title}`;
+        span_b1.innerText = "AUTHOR";
+        span_b2.innerText = `${book.author}`;
+        span_c1.innerText = "NUMBER OF PAGES";
+        span_c2.innerText = `${book.numberOfPages}`;
+
+        span_a1.classList.add("data");
+        span_b1.classList.add("data");
+        span_c1.classList.add("data");
+
+        span_a2.classList.add("value");
+        span_b2.classList.add("value");
+        span_c2.classList.add("value");
+
+        p1.appendChild(span_a1);
+        p1.appendChild(span_a2);
+        p2.appendChild(span_b1);
+        p2.appendChild(span_b2);
+        p3.appendChild(span_c1);
+        p3.appendChild(span_c2);
 
 
         let read_status = document.createElement("div");
         if(book.isRead){
-            read_status.innerText = `\u2705 Read`;
+            read_status.innerText = `\u2705READ`;
         }
         else{
-            read_status.innerText = "\u274C Unread";
+            read_status.innerText = "\u274CUNREAD";
         }
         read_status.classList.add("read-status");
 
         let buttons = document.createElement("div");
         let delButton = document.createElement("button");
-        delButton.innerText = "Delete";
+        delButton.innerText = "DELETE";
         delButton.classList.add("del-button");
         let read_status_button = document.createElement("button");
         if (book.isRead){
-            read_status_button.innerText = "Mark as Unread";
+            read_status_button.innerText = "MARK AS UNREAD";
         }
         else{
-            read_status_button.innerText = "Mark as Read";
+            read_status_button.innerText = "MARK AS READ";
         }
         read_status_button.classList.add("read-status-button");
 
@@ -102,7 +129,7 @@ function deleteBook(book){
 // function toggleReadStatus()
 
 
-const showBtn = document.querySelector("#add-book button");
+const showBtn = document.querySelector("#add-book-button");
 const closeBtn = dialog.querySelector("#closeBtn");
 const submitBtn = dialog.querySelector("#submitBtn");
 
@@ -110,24 +137,32 @@ showBtn.addEventListener("click", (e) => {
     dialog.showModal();
 });
 
-submitBtn.addEventListener("click", (e) => {
-    e.preventDefault();
+let bookIconsList = document.querySelectorAll(".book-icon");
+let bookIconArray = [...bookIconsList];
+bookIconArray.forEach(icon => {
+    icon.addEventListener("click", () => {
+        dialog.showModal();
+    })
+});
 
-    let at = document.querySelector("#author");
-    at.style.color = "red";
+
+
+submitBtn.addEventListener("click", (e) => {
     let author = dialog.querySelector("#author");
     let title = dialog.querySelector("#title");
     let pageCount = dialog.querySelector("#pages-count");
-    let readBefore = dialog.querySelector(('input[name = "book-read-status"]:checked'));
-
-
-    if ((author.value) && (title.value) && (pageCount.value > 0) && (readBefore.value)){
-        console.log(readBefore.value);
-        addBookToLibrary(title.value, author.value, pageCount.value, readBefore.value);
-        dialog.close();
-        console.log(myLibrary);
+    let readBefore = dialog.querySelector("input[name = book-read-status]:checked");
+    console.log(pageCount.value);
+    console.log(readBefore);
+    if (readBefore){
+        if ((author.value) && (title.value) && (pageCount.value > 0) && (readBefore.value)){
+            addBookToLibrary(title.value, author.value, pageCount.value, readBefore.value);
+            dialog.close();
+        }
     }
-})
+});
+
+
 
 
 
